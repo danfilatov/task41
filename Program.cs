feda1423d00dce818cf7db08e09237e25431fc3f
Denis Filatov <gooden@mail.ru>
2023-03-16 20:11:29 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите M чисел через запятую: ");
+//читаем числа с консоли заданных в одной строке
+//int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); 
+// пробелы, много пробелов
+//int[] array = Array.ConvertAll(Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries), (item) => Convert.ToInt32(item));
+// запятая
+int[] array = Array.ConvertAll(Console.ReadLine().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries), (item) => Convert.ToInt32(item));
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Чисел > 0: {count}");
